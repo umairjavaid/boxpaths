bd6e44a251d240f24515e754a6fafa1629136514
umairjavaid <bscs13024@itu.edu.pk>
2021-06-21 22:47:34 +0500

    Update vector_generator.py

--- vector_generator.py
+++ vector_generator.py
@@ -12,13 +12,16 @@ def get_info_from_str(string):
   frame_number = int(string.split()[5])
   return track_id, top_row, top_column, bottom_row, bottom_column, frame_number
 
+def get_mid_point(top_row, top_column, bottom_row, bottom_column):
+  row_mid = ((bottom_row - top_row)/2) + top_row)
+  col_mid = ((bottom_column - top_column)/2) + bottom_column)
+  return row_mid, col_mid
+
 def generate_vectors(arr):
   vector = []
   for i in arr:
-    track_id = i.split()[0]
-    top_left 
-    for j in arr:
-    
-        
+    track_id, top_row, top_column, bottom_row, bottom_column, frame_number = get_info_from_str(i)
+    mid_point = get_mid_point(top_row, top_column, bottom_row, bottom_column)
+            
 
 read_file("/content/boxpaths/direction_trainer_dataset.txt")
